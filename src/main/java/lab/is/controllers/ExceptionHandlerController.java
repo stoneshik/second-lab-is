903fd92acff134742a3dc6f3c7f1e01e118cca55
Big Floppa <92751564+stoneshik@users.noreply.github.com>
2025-11-29 16:22:36 +0300

    Initial commit

---
+++ src/main/java/lab/is/controllers/ExceptionHandlerController.java
@@ -0,0 +1,117 @@
+package lab.is.controllers;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpServerErrorException.InternalServerError;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import lab.is.dto.responses.ErrorMessageResponseDto;
+import lab.is.exceptions.IncorrectDtoInRequestException;
+import lab.is.exceptions.NestedObjectIsUsedException;
+import lab.is.exceptions.NestedObjectNotFoundException;
+import lab.is.exceptions.ObjectNotFoundException;
+import lab.is.exceptions.ValueOverflowException;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ErrorMessageResponseDto handleException(ObjectNotFoundException e) {
+        return ErrorMessageResponseDto.builder()
+            .timestamp(new Date())
+            .message(e.getMessage())
+            .build();
+    }
+
+    @ExceptionHandler(NestedObjectNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ErrorMessageResponseDto handleException(NestedObjectNotFoundException e) {
+        return ErrorMessageResponseDto.builder()
+            .timestamp(new Date())
+            .message(e.getMessage())
+            .build();
+    }
+
+    @ExceptionHandler(IncorrectDtoInRequestException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessageResponseDto handleException(IncorrectDtoInRequestException e) {
+        return ErrorMessageResponseDto.builder()
+            .timestamp(new Date())
+            .message(e.getMessage())
+            .build();
+    }
+
+    @ExceptionHandler(NestedObjectIsUsedException.class)
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    public ErrorMessageResponseDto handleException(NestedObjectIsUsedException e) {
+        return ErrorMessageResponseDto.builder()
+            .timestamp(new Date())
+            .message(e.getMessage())
+            .build();
+    }
+
+    @ExceptionHandler(ValueOverflowException.class)
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    public ErrorMessageResponseDto handleException(ValueOverflowException e) {
+        return ErrorMessageResponseDto.builder()
+            .timestamp(new Date())
+            .message(e.getMessage())
+            .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessageResponseDto handleException(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        Map<String, String> errors = new HashMap<>();
+        fieldErrors.forEach(fieldError ->
+            errors.put(
+                fieldError.getField(),
+                fieldError.getDefaultMessage()
+            )
+        );
+        return ErrorMessageResponseDto.builder()
+            .timestamp(new Date())
+            .message("Переданы неправильные значения")
+            .violations(errors)
+            .build();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessageResponseDto handleException(ConstraintViolationException e) {
+        Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+        Map<String, String> errors = new HashMap<>();
+        violations.forEach(violation ->
+            errors.put(
+                violation.getPropertyPath().toString(),
+                violation.getMessage()
+            )
+        );
+        return ErrorMessageResponseDto.builder()
+            .timestamp(new Date())
+            .message("Переданы неправильные значения")
+            .violations(errors)
+            .build();
+    }
+
+    @ExceptionHandler(InternalServerError.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorMessageResponseDto handleException(InternalServerError e) {
+        return ErrorMessageResponseDto.builder()
+            .timestamp(new Date())
+            .message("Ошибка сервера")
+            .build();
+    }
+}
---
+++ src/main/java/lab/is/services/musicband/MusicBandService.java
@@ -0,0 +1,111 @@
+package lab.is.services.musicband;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lab.is.bd.entities.Coordinates;
+import lab.is.bd.entities.MusicBand;
+import lab.is.bd.entities.MusicGenre;
+import lab.is.dto.requests.musicband.MusicBandRequestCreateDto;
+import lab.is.dto.requests.musicband.MusicBandRequestUpdateDto;
+import lab.is.dto.responses.musicband.MusicBandResponseDto;
+import lab.is.dto.responses.musicband.WrapperListMusicBandResponseDto;
+import lab.is.repositories.MusicBandRepository;
+import lab.is.repositories.specifications.musicband.MusicBandSpecifications;
+import lab.is.util.musicband.MusicBandToDtoFromEntityMapper;
+import lab.is.util.musicband.MusicBandToEntityFromDtoCreateRequest;
+import lab.is.util.musicband.MusicBandToEntityFromDtoUpdateRequest;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MusicBandService {
+    private final MusicBandRepository musicBandRepository;
+    private final MusicBandTxService musicBandTxService;
+    private final MusicBandSpecifications musicBandSpecifications;
+    private final MusicBandToEntityFromDtoCreateRequest musicBandToEntityFromDtoCreateRequest;
+    private final MusicBandToEntityFromDtoUpdateRequest musicBandToEntityFromDtoUpdateRequest;
+
+    @Transactional(readOnly = true)
+    public WrapperListMusicBandResponseDto findAll(
+        String name,
+        MusicGenre genre,
+        String description,
+        String bestAlbumName,
+        String studioName,
+        String studioAddress,
+        Pageable pageable
+    ) {
+        Specification<MusicBand> specification = Specification.unrestricted();
+
+        specification = specification.and(musicBandSpecifications.nameLike(name));
+        specification = specification.and(musicBandSpecifications.genreEquals(genre));
+        specification = specification.and(musicBandSpecifications.descriptionLike(description));
+        specification = specification.and(musicBandSpecifications.bestAlbumNameLike(bestAlbumName));
+        specification = specification.and(musicBandSpecifications.studioNameLike(studioName));
+        specification = specification.and(musicBandSpecifications.studioAddressLike(studioAddress));
+
+        Page<MusicBand> page = musicBandRepository.findAll(specification, pageable);
+        List<MusicBandResponseDto> musicBandResponseDtos = new ArrayList<>();
+
+        page.forEach(musicBand ->
+            musicBandResponseDtos.add(
+                MusicBandToDtoFromEntityMapper.toDtoFromEntity(musicBand)
+            )
+        );
+
+        return WrapperListMusicBandResponseDto.builder()
+            .totalElements(page.getTotalElements())
+            .totalPages(page.getTotalPages())
+            .currentPage(page.getNumber())
+            .pageSize(page.getNumberOfElements())
+            .musicBands(musicBandResponseDtos)
+            .build();
+    }
+
+    @Transactional(readOnly = true)
+    public MusicBandResponseDto findById(Long id) {
+        MusicBand musicBand = musicBandTxService.findByIdReturnsEntity(id);
+        return MusicBandToDtoFromEntityMapper.toDtoFromEntity(musicBand);
+    }
+
+    @Transactional
+    public MusicBand create(MusicBandRequestCreateDto dto) {
+        MusicBand musicBand = musicBandToEntityFromDtoCreateRequest.toEntityFromDto(dto);
+        MusicBand savedMusicBand = musicBandRepository.save(musicBand);
+        musicBandRepository.flush();
+        return savedMusicBand;
+    }
+
+    @Transactional
+    public MusicBand update(long id, MusicBandRequestUpdateDto dto) {
+        MusicBand musicBand = musicBandTxService.findByIdReturnsEntity(id);
+        musicBand = musicBandToEntityFromDtoUpdateRequest.toEntityFromDto(
+            dto,
+            musicBand
+        );
+        MusicBand savedMusicBand = musicBandRepository.save(musicBand);
+        musicBandRepository.flush();
+        return savedMusicBand;
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        MusicBand musicBand = musicBandTxService.findByIdReturnsEntity(id);
+        Coordinates coordinates = musicBand.getCoordinates();
+        coordinates.removeMusicBand(musicBand);
+        musicBandRepository.delete(musicBand);
+        musicBandRepository.flush();
+    }
+
+    @Transactional(readOnly = true)
+    public MusicBand findByIdReturnsEntity(Long id) {
+        return musicBandTxService.findByIdReturnsEntity(id);
+    }
+}
